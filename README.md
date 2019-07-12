38d4c63595f855bf36f06771c331ede48bbb72ed
KuribohG <zouyuheng1998@foxmail.com>
2019-07-13 02:33:46 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FrostFire
+
+Developed with Unreal Engine 4
---
+++ Source/FrostFire/FrostFireGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FrostFireGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FROSTFIRE_API AFrostFireGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
